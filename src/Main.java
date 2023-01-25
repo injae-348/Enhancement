2919939cdfa8f8dfa9b3a0f951b529ed1937aaca
Injae Jeong <ant0593@naver.com>
2023-01-25 21:07:36 +0900

    Add hammer system

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,6 @@ import java.io.InputStreamReader;
 //import java.io.OutputStreamWriter;
 import java.io.IOException;
 
-
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -13,23 +12,79 @@ public class Main {
 		System.out.print("강화하시겠습니까? (Y/N)\nY = yes, N = no \ny 또는 n을 입력해주세요 : ");
 		int k = br.read();	// y또는 Y의 아스키 코드면 실행
 		if(k==89 || k==121) {
-			obj = new Object1();
-			obj.Print();
 			br.readLine();
+			System.out.println("강화할 단계를 입력해주세요 ( 0강 ~ 13강 )");
+			k = Integer.parseInt(br.readLine());
+			obj = new Object1(k);
+			obj.Print();
+			obj.setHammer(true);
 			System.out.println();
-			
 			String stop;
-			while((stop = br.readLine())!=null) {
+			while(true) {
+				br.readLine();
+				System.out.print("강화를 계속할거면 아무키를, 종료하실거면 stop을 입력해주세요");
+				stop = br.readLine();
 				if(stop.equals("stop")) break;
 				if(obj.getLevel()==13) {
 					System.out.println("강화 최고 단계입니다.");
 					break;
 				}
-				
-				obj.Enhancement();
-				// obj.FluEnhancement();
-				
-				obj.Print();
+						
+				if(obj.getLevel()>=11) {
+					System.out.print("헤머를 사용하시겠어요?");
+					k = br.read();
+					if(k==89 || k==121) {
+						obj.setHammer(true);
+					}
+					else {
+						obj.setHammer(false);
+					}
+					br.readLine();
+					System.out.print("플루오르 수정을 사용하시겠어요?");
+					k = br.read();
+					if(k==89 || k==121) {
+						obj.FluCrystalEnhancement();
+						obj.Print();
+					}
+					else {
+						obj.Enhancement();
+						obj.Print();
+					}
+				}
+				else if(obj.getLevel()>=7) {
+					System.out.print("헤머를 사용하시겠어요?");
+					k = br.read();
+					if(k==89 || k==121) {
+						obj.setHammer(true);
+					}
+					else {
+						obj.setHammer(false);
+					}
+					br.readLine();
+					System.out.print("플루오르 스톤을 사용하시겠어요?");
+					k = br.read();
+					if(k==89 || k==121) {
+						obj.FluStoneEnhancement();
+						obj.Print();	
+					}
+					else {
+						obj.Enhancement();
+						obj.Print();
+					}	
+				}
+				else {
+					System.out.print("헤머를 사용하시겠어요?");
+					k = br.read();
+					if(k==89 || k==121) {
+						obj.setHammer(true);
+					}
+					else {
+						obj.setHammer(false);
+					}
+					br.readLine();
+					obj.Enhancement();
+					obj.Print();
+				}
 				System.out.println();
 			}
 			obj.Print();
@@ -40,6 +95,8 @@ public class Main {
 			System.out.println("강화를 안합니다.");
 		}
 		
-		
+		System.out.println("플루오르 스톤 사용 개수 : "+obj.getFluStoneCnt());
+		System.out.println("복원의 주문서 사용 개수 : "+obj.getRestoreCnt());
+		System.out.println("플루오르 수정 사용 개수 : "+obj.getFluCrystalCnt());
 	}
 }
--- src/Object1.java
+++ src/Object1.java
@@ -5,23 +5,29 @@ public class Object1 {
 	private int level;
 	private int count;
 	private int restoreCnt;
-	private int fluCnt;	// 스톤이랑 수정 분리 필요
-	private boolean isDestruction;
-
+	private int fluStoneCnt;	// 스톤이랑 수정 분리 필요
+	private int fluCrystalCnt;
+	//private boolean isDestruction;
+	private boolean hammer;
+	
 	Object1(){
 		level = 0;
 		restoreCnt=0;
-		fluCnt=0;
+		fluStoneCnt=0;
+		fluCrystalCnt=0;
 		setCount(0);
-		setDestruction(true);
+		//setDestruction(true);
+		hammer = false;
 	}
 	
 	Object1(int level){
-		level = this.level;
+		this.level = level;
 		restoreCnt=0;
-		fluCnt=0;
+		fluStoneCnt=0;
+		fluCrystalCnt=0;
 		setCount(0);
-		setDestruction(true);
+		//setDestruction(true);
+		hammer = false;
 	}
 	
 	public void Success() {
@@ -39,7 +45,7 @@ public class Object1 {
 	}
 	
 	public void Destroy() {
-		setDestruction(false);
+		//setDestruction(false);
 		System.out.println("파괴 되었습니다");
 		restoreCnt++;
 	}
@@ -55,308 +61,24 @@ public class Object1 {
 		return restoreCnt;
 	}
 	
-	public int getFluCnt() {
-		return fluCnt;
+	public int getFluStoneCnt() {
+		return fluStoneCnt;
 	}
 	
-	public boolean isDestruction() {
-		return isDestruction;
+	public int getFluCrystalCnt() {
+		return fluCrystalCnt;
 	}
+
+	/*
+	 * public boolean isDestruction() { return isDestruction; }
 
 	private void setDestruction(boolean isDestruction) {
 		this.isDestruction = isDestruction;
 	}
+	 */
 	
 	public int getLevel() {
 		return level;
-	}
-	
-	public void Enhancement() {
-		// 함수로 좀더 간단하게 압축시키기		
-		int rand = getRandomInt();
-		setCount(getCount() + 1);
-		if(level==0 || level==1) {
-			Success();
-		}
-		else if(level == 2) {
-			if(rand<=9000 && rand>0) {
-				Success();
-			}
-			else {
-				NoChange();
-			}
-		}
-		else if(level == 3) {
-			if(rand<=8000 && rand>0) {
-				Success();
-			}
-			else {
-				NoChange();
-			}
-		}
-		else if(level == 4) {
-			if(rand<=7000 && rand>0) {
-				Success();
-			}
-			else {
-				NoChange();
-			}
-		}
-		else if(level == 5) {
-			if(rand<=6000 && rand>0) {
-				Success();
-			}
-			else {
-				NoChange();
-			}
-		}
-		else if(level == 6) {
-			if(rand<=2000 && rand>0) {
-				Success();
-			}
-			else {
-				NoChange();
-			}
-		}
-		else if(level == 7) {
-			if(rand<=500 && rand>0) {
-				Success();
-			}
-			else if(rand>500 && rand<=2000) {
-				NoChange();
-			}
-			else if(rand>2000 && rand<=5000) {
-				Drop();
-			}
-			else {
-				Initialization();
-			}
-		}
-		else if(level == 8) {
-			if(rand<=200 && rand>0) {
-				Success();
-			}
-			else if(rand>200 && rand<=1200) {
-				NoChange();
-			}
-			else if(rand>1200 && rand<=4000) {
-				Drop();
-			}
-			else if(rand>4000 && rand<=6000) {
-				Destroy();
-			}
-			else {
-				Initialization();
-			}
-		}
-		else if(level == 9) {
-			if(rand<=100 && rand>0) {
-				Success();
-			}
-			else if(rand>100 && rand<=2800) {
-				Drop();
-			}
-			else if(rand>2800 && rand<=3500) {
-				NoChange();
-			}
-			else if(rand>3500 && rand<=6000) {
-				Destroy();
-			}
-			else {
-				Initialization();
-			}
-		}
-		else if(level == 10) {
-			if(rand<=70 && rand>0) {
-				Success();
-			}
-			else if(rand>70 && rand<=3600) {
-				Destroy();
-			}
-			else if(rand>3600 && rand<=4300) {
-				NoChange();
-			}
-			else if(rand>4300 && rand<=7000) {
-				Drop();
-			}
-			else {
-				Initialization();
-			}
-		}
-		else if(level == 11) {
-			if(rand<=100 && rand>0) {
-				Success();
-			}
-			else if(rand>100 && rand<=4800) {
-				NoChange();
-			}
-			else if(rand>4800 && rand<=7500) {
-				Drop();
-			}
-			else {
-				Destroy();
-			}
-		}
-		else if(level == 12) {
-			if(rand<=100 && rand>0) {
-				Success();
-			}
-			else if(rand>100 && rand<=4400) {
-				NoChange();
-			}
-			else if(rand>4400 && rand<=7100) {
-				Drop();
-			}
-			else {
-				Destroy();
-			}
-		}
-	}
-	
-	public void FluEnhancement() {
-		// 함수로 좀더 간단하게 압축시키기		
-		fluCnt++;
-		int rand = getRandomInt();
-		setCount(getCount() + 1);
-		if(level==0 || level==1) {
-			Success();
-		}
-		else if(level == 2) {
-			if(rand<=9000 && rand>0) {
-				Success();
-			}
-			else {
-				NoChange();
-			}
-		}
-		else if(level == 3) {
-			if(rand<=8000 && rand>0) {
-				Success();
-			}
-			else {
-				NoChange();
-			}
-		}
-		else if(level == 4) {
-			if(rand<=7000 && rand>0) {
-				Success();
-			}
-			else {
-				NoChange();
-			}
-		}
-		else if(level == 5) {
-			if(rand<=6000 && rand>0) {
-				Success();
-			}
-			else {
-				NoChange();
-			}
-		}
-		else if(level == 6) {
-			if(rand<=2000 && rand>0) {
-				Success();
-			}
-			else {
-				NoChange();
-			}
-		}
-		else if(level == 7) {
-			if(rand<=500 && rand>0) {
-				Success();
-			}
-			else {
-				NoChange();
-			}
-			
-		}
-		else if(level == 8) {
-			if(rand<=200 && rand>0) {
-				Success();
-			}
-			else if(rand>200 && rand<=8000) {
-				NoChange();
-			}
-			else {
-				Destroy();
-			}
-			
-		}
-		else if(level == 9) {
-			if(rand<=100 && rand>0) {
-				Success();
-			}
-			else if(rand>100 && rand<=7500) {
-				NoChange();
-			}
-			else {
-				Destroy();
-			}
-			
-		}
-		else if(level == 10) {
-			if(rand<=70 && rand>0) {
-				Success();
-			}
-			else if(rand>70 && rand<=3600) {
-				Destroy();
-			}
-			else {
-				NoChange();
-			}
-		}
-		else if(level == 11) {
-			if(rand<=100 && rand>0) {
-				Success();
-			}
-			else if(rand>100 && rand<=2800) {
-				Drop();
-			}
-			else if(rand>2800 && rand<=3500) {
-				NoChange();
-			}
-			else if(rand>3500 && rand<=6000) {
-				Destroy();
-			}
-			else {
-				Initialization();
-			}
-		}
-		else if(level == 12) {
-			if(rand<=100 && rand>0) {
-				Success();
-			}
-			else if(rand>100 && rand<=2800) {
-				Drop();
-			}
-			else if(rand>2800 && rand<=3100) {
-				NoChange();
-			}
-			else if(rand>3100 && rand<=6000) {
-				Destroy();
-			}
-			else {
-				Initialization();
-			}
-		}
-	
-	}
-	
-	private int getRandomInt() {
-		Random random = new Random();
-		random.setSeed(System.currentTimeMillis());
-		return random.nextInt(100000)+1;
-	}
-	
-	public void Print() {
-		System.out.println("현재 상태");
-		System.out.println("강화 단계 : " + level);
-		if(isDestruction==true) {
-			System.out.println("파괴 여부 : " + "강화 가능");			
-		}
-		else {
-			System.out.println("파괴 여부 : " + "파괴");				
-		}
 	}
 
 	public int getCount() {
@@ -365,5 +87,462 @@ public class Object1 {
 
 	public void setCount(int count) {
 		this.count = count;
+	}	
+	
+	public void setHammer(boolean a) {
+		hammer = a;
 	}
+	
+	public void Enhancement() {
+		// 함수로 좀더 간단하게 압축시키기		
+		int rand = getRandomInt();
+		setCount(getCount() + 1);
+		
+		if(hammer==false) {
+			if(level==0 || level==1) {
+				Success();
+			}
+			else if(level == 2) {
+				if(rand<=9000 && rand>0) {
+					Success();
+				}
+				else {
+					NoChange();
+				}
+			}
+			else if(level == 3) {
+				if(rand<=8000 && rand>0) {
+					Success();
+				}
+				else {
+					NoChange();
+				}
+			}
+			else if(level == 4) {
+				if(rand<=7000 && rand>0) {
+					Success();
+				}
+				else {
+					NoChange();
+				}
+			}
+			else if(level == 5) {
+				if(rand<=6000 && rand>0) {
+					Success();
+				}
+				else {
+					NoChange();
+				}
+			}
+			else if(level == 6) {
+				if(rand<=2000 && rand>0) {
+					Success();
+				}
+				else {
+					NoChange();
+				}
+			}
+			else if(level == 7) {
+				if(rand<=500 && rand>0) {
+					Success();
+				}
+				else if(rand>500 && rand<=2000) {
+					NoChange();
+				}
+				else if(rand>2000 && rand<=5000) {
+					Drop();
+				}
+				else {
+					Initialization();
+				}
+			}
+			else if(level == 8) {
+				if(rand<=200 && rand>0) {
+					Success();
+				}
+				else if(rand>200 && rand<=2200) {
+					Destroy();
+				}
+				else if(rand>2200 && rand<=3200) {
+					NoChange();
+				}
+				else if(rand>3200 && rand<=6000) {
+					Drop();
+				}
+				else {
+					Initialization();
+				}
+			}
+			else if(level == 9) {
+				if(rand<=100 && rand>0) {
+					Success();
+				}
+				else if(rand>100 && rand<=2600) {
+					Destroy();
+				}
+				else if(rand>2600 && rand<=3300) {
+					NoChange();
+				}
+				else if(rand>3300 && rand<=6000) {
+					Drop();
+				}
+				else {
+					Initialization();
+				}
+			}
+			else if(level == 10) {
+				if(rand<=70 && rand>0) {
+					Success();
+				}
+				else if(rand>70 && rand<=3600) {
+					Destroy();
+				}
+				else if(rand>3600 && rand<=4300) {
+					NoChange();
+				}
+				else if(rand>4300 && rand<=7000) {
+					Drop();
+				}
+				else {
+					Initialization();
+				}
+			}
+			else if(level == 11) {
+				if(rand<=100 && rand>0) {
+					Success();
+				}
+				else if(rand>100 && rand<=2600) {
+					Destroy();
+				}
+				else if(rand>2600 && rand<=3300) {
+					NoChange();
+				}
+				else if(rand>3300 && rand<=6000){
+					Drop();
+				}
+				else {
+					Initialization();
+				}
+			}
+			else if(level == 12) {
+				if(rand<=100 && rand>0) {
+					Success();
+				}
+				else if(rand>100 && rand<=3000) {
+					Destroy();
+				}
+				else if(rand>3000 && rand<=3300) {
+					NoChange();
+				}
+				else if(rand>3300 && rand<=6000){
+					Drop();
+				}
+				else {
+					Initialization();
+				}
+			}
+		}
+		else {
+			if(level>=0 && level<6) {
+				Success();
+			}	
+			else if(level == 6) {
+				if(rand<=4000 && rand>0) {
+					Success();
+				}
+				else {
+					NoChange();
+				}
+			}
+			else if(level == 7) {
+				if(rand<=1000 && rand>0) {
+					Success();
+				}
+				else if(rand>1000 && rand<=2000) {
+					NoChange();
+				}
+				else if(rand>2000 && rand<=5000) {
+					Drop();
+				}
+				else {
+					Initialization();
+				}
+			}
+			else if(level == 8) {
+				if(rand<=400 && rand>0) {
+					Success();
+				}
+				else if(rand>400 && rand<=2400) {
+					Destroy();
+				}
+				else if(rand>2400 && rand<=3200) {
+					NoChange();
+				}
+				else if(rand>3200 && rand<=6000) {
+					Drop();
+				}
+				else {
+					Initialization();
+				}
+			}
+			else if(level == 9) {
+				if(rand<=200 && rand>0) {
+					Success();
+				}
+				else if(rand>200 && rand<=2700) {
+					Destroy();
+				}
+				else if(rand>2700 && rand<=3300) {
+					NoChange();
+				}
+				else if(rand>3300 && rand<=6000) {
+					Drop();
+				}
+				else {
+					Initialization();
+				}
+			}
+			else if(level == 10) {
+				if(rand<=140 && rand>0) {
+					Success();
+				}
+				else if(rand>140 && rand<=3670) {
+					Destroy();
+				}
+				else if(rand>3670 && rand<=4300) {
+					NoChange();
+				}
+				else if(rand>4300 && rand<=7000) {
+					Drop();
+				}
+				else {
+					Initialization();
+				}
+			}
+			else if(level == 11) {
+				if(rand<=200 && rand>0) {
+					Success();
+				}
+				else if(rand>200 && rand<=2700) {
+					Destroy();
+				}
+				else if(rand>2700 && rand<=3300) {
+					NoChange();
+				}
+				else if(rand>3300 && rand<=6000){
+					Drop();
+				}
+				else {
+					Initialization();
+				}
+			}
+			else if(level == 12) {
+				if(rand<=200 && rand>0) {
+					Success();
+				}
+				else if(rand>200 && rand<=3100) {
+					Destroy();
+				}
+				else if(rand>3100 && rand<=3300) {
+					NoChange();
+				}
+				else if(rand>3300 && rand<=6000){
+					Drop();
+				}
+				else {
+					Initialization();
+				}
+			}
+		}
+	}
+	
+	public void FluStoneEnhancement() {
+		// 함수로 좀더 간단하게 압축시키기		
+		fluStoneCnt++;
+		int rand = getRandomInt();
+		setCount(getCount() + 1);
+		//System.out.print(rand);
+		if(hammer == false) {			
+			if(level == 7) {
+				if(rand<=500 && rand>0) {
+					Success();
+				}
+				else {
+					NoChange();
+				}
+				
+			}
+			else if(level == 8) {
+				if(rand<=200 && rand>0) {
+					Success();
+				}
+				else if(rand>200 && rand<=2200) {
+					Destroy();		
+				}
+				else {
+					NoChange();
+				}
+			}
+			else if(level == 9) {
+				if(rand<=100 && rand>0) {
+					Success();
+				}
+				else if(rand>100 && rand<=2600) {
+					Destroy();
+				}
+				else {
+					NoChange();
+				}
+				
+			}
+			else if(level == 10) {
+				if(rand<=70 && rand>0) {
+					Success();
+				}
+				else if(rand>70 && rand<=3600) {
+					Destroy();
+				}
+				else {
+					NoChange();
+				}
+			}
+		}
+		else {
+			if(level == 7) {
+				if(rand<=1000 && rand>0) {
+					Success();
+				}
+				else {
+					NoChange();
+				}
+				
+			}
+			else if(level == 8) {
+				if(rand<=400 && rand>0) {
+					Success();
+				}
+				else if(rand>400 && rand<=2400) {
+					Destroy();		
+				}
+				else {
+					NoChange();
+				}
+			}
+			else if(level == 9) {
+				if(rand<=200 && rand>0) {
+					Success();
+				}
+				else if(rand>200 && rand<=2700) {
+					Destroy();
+				}
+				else {
+					NoChange();
+				}
+				
+			}
+			else if(level == 10) {
+				if(rand<=140 && rand>0) {
+					Success();
+				}
+				else if(rand>140 && rand<=3670) {
+					Destroy();
+				}
+				else {
+					NoChange();
+				}
+			}
+		}
+	}
+	
+	public void FluCrystalEnhancement() {
+		int rand = getRandomInt();
+		setCount(getCount() + 1);
+		fluCrystalCnt++;
+		
+		if(hammer==false) {			
+			if(level == 11) {
+				if(rand<=100 && rand>0) {
+					Success();
+				}
+				else if(rand>100 && rand<=2600) {
+					Destroy();
+				}
+				else if(rand>2600 && rand<=3300) {
+					NoChange();
+				}
+				else if(rand>3300 && rand<=6000) {
+					Drop();
+				}
+				else {
+					NoChange();
+				}
+			}
+			else if(level == 12) {
+				if(rand<=100 && rand>0) {
+					Success();
+				}
+				else if(rand>100 && rand<=3000) {
+					Destroy();
+				}
+				else if(rand>3000 && rand<=3300) {
+					NoChange();
+				}
+				else if(rand>3300 && rand<=6000) {
+					Drop();
+				}
+				else {
+					NoChange();
+				}
+			}	
+		}
+		else {
+			if(level == 11) {
+				if(rand<=200 && rand>0) {
+					Success();
+				}
+				else if(rand>200 && rand<=2700) {
+					Destroy();
+				}
+				else if(rand>2700 && rand<=3300) {
+					NoChange();
+				}
+				else if(rand>3300 && rand<=6000) {
+					Drop();
+				}
+				else {
+					NoChange();
+				}
+			}
+			else if(level == 12) {
+				if(rand<=200 && rand>0) {
+					Success();
+				}
+				else if(rand>200 && rand<=3100) {
+					Destroy();
+				}
+				else if(rand>3100 && rand<=3300) {
+					NoChange();
+				}
+				else if(rand>3300 && rand<=6000) {
+					Drop();
+				}
+				else {
+					NoChange();
+				}
+			}		
+		}
+	}
+	
+	private int getRandomInt() {
+		Random random = new Random();
+		random.setSeed(System.currentTimeMillis());
+		return random.nextInt(10000)+1;
+	}
+	
+	public void Print() {
+		System.out.println("현재 상태");
+		System.out.println("강화 단계 : " + level);
+	}
+
+	
 }
